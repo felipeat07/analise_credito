c040a54f3e35bccf6541a4d34092603f394df2c0
Felipe Tavares <felipeat07@gmail.com>
2023-07-14 13:32:22 -0300

    Initial commit

---
+++ analise_credito.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+base_credit = pd.read_csv('credit_data.csv')
+
+base_credit.describe()
+
+np.unique(base_credit['default'])
