84e09e9c40de2fbbad7e60042684abe45df0e5e3
Felipe Tavares <felipeat07@gmail.com>
2023-07-28 22:55:37 -0300

    use yellowbrick to view data am census dataset

--- naive_bayes/naive_bayes_base_census.py
+++ naive_bayes/naive_bayes_base_census.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import pickle
+from yellowbrick.classifier import ConfusionMatrix
+
 
 with open('../pre_processamento_dados/census.pkl', 'rb') as f:
     X_census_treinamento, Y_census_treinamento, X_census_teste, Y_census_teste = pickle.load(f)
@@ -12,3 +14,9 @@ naive_census.fit(X_census_treinamento, Y_census_treinamento)
 previsoes = naive_census.predict(X_census_teste)
 
 accuracy_score(Y_census_teste, previsoes)
+
+cm = ConfusionMatrix(naive_census)
+cm.fit(X_census_treinamento, Y_census_treinamento)
+cm.score(X_census_teste, Y_census_teste)
+
+print(classification_report(Y_census_teste, previsoes))
